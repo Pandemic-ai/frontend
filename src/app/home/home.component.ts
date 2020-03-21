e4970abe7617921fe56acce0f3538cfe22643499
RahulRaj <rajr97333@gmail.com>
2020-03-21 21:42:01 +0100

    add search on map

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,28 +1,28 @@
-import {Component, OnInit} from '@angular/core';
-import * as L from 'leaflet';
+import { Component, OnInit } from "@angular/core";
+import * as L from "leaflet";
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-    private map;
+  private map;
 
-    constructor() {
-    }
+  constructor() {}
 
-    ngOnInit() {
-        this.initMap();
-    }
+  ngOnInit() {
+    this.initMap();
+  }
 
+  private initMap(): void {
+    this.map = L.map("map").setView([46.879966, -121.726909], 3);
 
-    private initMap(): void {
-        this.map = L.map('map').setView([46.879966, -121.726909], 3);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution:
-                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(this.map);
-    }
+    // this.map.fitBounds(poly.getBounds()); //to fit and get bounds
+  }
 }
