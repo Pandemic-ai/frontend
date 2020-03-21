0707ed40fdde363fe6119b64dd1ddb1b505bfcc2
RahulRaj <rajr97333@gmail.com>
2020-03-22 00:58:48 +0100

    added data to map

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,13 +16,68 @@ export class HomeComponent implements OnInit {
   }
 
   private initMap(): void {
-    this.map = L.map("map").setView([46.879966, -121.726909], 3);
+    var request = new XMLHttpRequest();
+    var token = "F9bQK456iUpJVZJLTZsMEKhhENqnGJ";
 
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(this.map);
+    request.open(
+      "GET",
+      "https://coronaviva.herokuapp.com/api/1/cronavirues/area/"
+    );
 
-    // this.map.fitBounds(poly.getBounds()); //to fit and get bounds
+    var pointsDataMarkers = Array();
+
+    window.onload = function() {
+      request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var data = JSON.parse(this.responseText);
+
+          var defaultZoom = 4;
+          var defaultCenter = [52.215933, 19.134422];
+          var map = L.map("map", {
+            center: defaultCenter,
+            zoom: defaultZoom,
+            maxZoom: 20,
+            minZoom: 2
+          });
+
+          L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution:
+              '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          }).addTo(map);
+
+          map.addLayer(pointsLayer);
+
+          console.log(data);
+          data.forEach(point => {
+            var don = L.marker([point.lat, point.long]);
+            don
+              .addTo(map)
+              .bindPopup(
+                "<b>Country:</b> " +
+                  point.country +
+                  "<br />" +
+                  " <b>Confirmed Case:</b> " +
+                  point.totalConfirmed +
+                  "<br />" +
+                  " <b>Death Cases:</b> " +
+                  point.totalDeaths +
+                  "<br />" +
+                  " <b>Recovered Cases:</b> " +
+                  point.totalRecovered
+              );
+
+            // map.fitBounds(don.getBounds());
+
+            // marker.bindPopup(display_text);
+            // pointsDataMarkers.push(marker);
+          });
+        }
+      };
+
+      var pointsLayer = L.layerGroup(pointsDataMarkers);
+      request.setRequestHeader("Content-Type", "application/json");
+      request.setRequestHeader("Authorization", "Bearer " + token);
+      request.send();
+    };
   }
 }
